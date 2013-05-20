b7ec02a14201535d6cb147c77252e172da0aec5d
Administrador <Administrador@FER-PC>
2013-05-20 16:10:15 -0400

    diseño de css

---
+++ index.php
@@ -0,0 +1,39 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/libs/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>        
+        <section class="wrapper ">
+            <header class=" header">
+                <h1 class="logo">UDABOL</h1>                                
+                <div class="user">
+                    <div class="izq"></div>
+                    <div>
+                        <img src="images/sin_foto.gif" width="48" height="48" alt="sin_foto"/>                        
+                        fernando mendoza
+                    </div>
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="">asdf</a></li>
+                        <li><a href="">asdf</a></li>
+                        <li><a href="">asdf</a></li>                                                                                                                                                 
+                    </ul>
+                </nav>                
+            </header>
+            <section class="section" >                          
+                <aside>
+                </aside>
+            </section>
+            <footer class="footer"><div>Todos los Derechos Reservados, Udabol © 2013</div></footer>
+        </section>
+    </body>
+</html>
--- my_error.php
+++ my_error.php
@@ -4,6 +4,7 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title></title>
         <link type="text/css" rel="stylesheet" href="main.css" media="all">
+        <script src="js/libs/modernizr-2.6.2.min.js" ></script>
     </head>
     <body>
         <section class="container">
