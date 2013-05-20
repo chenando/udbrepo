f1c7338d2c424915a843a15ab5190302e6aa70a7
Administrador <Administrador@FER-PC>
2013-05-20 18:47:07 -0400

    diseño de css v.1.2

--- index.php
+++ index.php
@@ -14,8 +14,7 @@ and open the template in the editor.
         <section class="wrapper ">
             <header class=" header">
                 <h1 class="logo">UDABOL</h1>                                
-                <div class="user">
-                    <div class="izq"></div>
+                <div class="user">                  
                     <div>
                         <img src="images/sin_foto.gif" width="48" height="48" alt="sin_foto"/>                        
                         fernando mendoza
@@ -23,15 +22,229 @@ and open the template in the editor.
                 </div>
                 <nav>
                     <ul>
-                        <li><a href="">asdf</a></li>
-                        <li><a href="">asdf</a></li>
-                        <li><a href="">asdf</a></li>                                                                                                                                                 
+                        <li><a href="">asdf asdf asdf</a></li>
+                        <li><a href="">asdf asdf asdf</a></li>
+                        <li><a href="">asdf asdf asdf</a></li>                                                
                     </ul>
-                </nav>                
+                </nav>   
             </header>
             <section class="section" >                          
-                <aside>
-                </aside>
+                <div>
+                    <table class="tablaLista">
+                        <thead>
+                            <tr>
+                                <th>EMPLEADO</th>
+                                <th>SEDE</th>
+                                <th>CARRERA</th>
+                                <th>NOMBRE_CARRERA</th>
+                                <th>MATERIA</th>
+                                <th>SIGLA_MATERIA</th>
+                                <th>NOMBRE_MATERIA</th>
+                                <th>GRUPO_ASIGNATURA</th>
+                                <th>TIPO_GRUPO</th>
+                                <th>NOMBRE_TIPO_GRUPO</th>
+                                <th>OPERACION</th>                
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1005</td>
+                                <td>SC</td>
+                                <td>57</td>
+                                <td>ENFERMERIA</td>
+                                <td>4626</td>
+                                <td>MOR-112</td>
+                                <td>MORFOLOGÍA I</td>
+                                <td>A</td>
+                                <td>NORE</td>
+                                <td>REGULAR</td>
+                                <td><a href="http://sis04.udabol.com/ingreso_nota/actas/ingresar/SC/57/4626/A/NORE">ingresar notas</a></td>                                
+                            </tr>     
+                            <tr>
+                                <td>1005</td>
+                                <td>SC</td>
+                                <td>57</td>
+                                <td>ENFERMERIA</td>
+                                <td>4626</td>
+                                <td>MOR-112</td>
+                                <td>MORFOLOGÍA I</td>
+                                <td>A1</td>
+                                <td>PRNO</td>
+                                <td>PRACTICO REGULAR</td>
+                                <td><a href="http://sis04.udabol.com/ingreso_nota/actas/ingresar/SC/57/4626/A1/PRNO">ingresar notas</a></td>                                
+                            </tr>     
+                            <tr>
+                                <td>1005</td>
+                                <td>SC</td>
+                                <td>57</td>
+                                <td>ENFERMERIA</td>
+                                <td>4626</td>
+                                <td>MOR-112</td>
+                                <td>MORFOLOGÍA I</td>
+                                <td>A2</td>
+                                <td>PRNO</td>
+                                <td>PRACTICO REGULAR</td>
+                                <td><a href="http://sis04.udabol.com/ingreso_nota/actas/ingresar/SC/57/4626/A2/PRNO">ingresar notas</a></td>                                
+                            </tr>     
+                            <tr>
+                                <td>1005</td>
+                                <td>SC</td>
+                                <td>57</td>
+                                <td>ENFERMERIA</td>
+                                <td>4626</td>
+                                <td>MOR-112</td>
+                                <td>MORFOLOGÍA I</td>
+                                <td>B</td>
+                                <td>NORE</td>
+                                <td>REGULAR</td>
+                                <td><a href="http://sis04.udabol.com/ingreso_nota/actas/ingresar/SC/57/4626/B/NORE">ingresar notas</a></td>                                
+                            </tr>     
+                            <tr>
+                                <td>1005</td>
+                                <td>SC</td>
+                                <td>57</td>
+                                <td>ENFERMERIA</td>
+                                <td>4626</td>
+                                <td>MOR-112</td>
+                                <td>MORFOLOGÍA I</td>
+                                <td>B1</td>
+                                <td>PRNO</td>
+                                <td>PRACTICO REGULAR</td>
+                                <td><a href="http://sis04.udabol.com/ingreso_nota/actas/ingresar/SC/57/4626/B1/PRNO">ingresar notas</a></td>                                
+                            </tr>     
+                            <tr>
+                                <td>1005</td>
+                                <td>SC</td>
+                                <td>57</td>
+                                <td>ENFERMERIA</td>
+                                <td>4626</td>
+                                <td>MOR-112</td>
+                                <td>MORFOLOGÍA I</td>
+                                <td>B2</td>
+                                <td>PRNO</td>
+                                <td>PRACTICO REGULAR</td>
+                                <td><a href="http://sis04.udabol.com/ingreso_nota/actas/ingresar/SC/57/4626/B2/PRNO">ingresar notas</a></td>                                
+                            </tr>     
+                            <tr>
+                                <td>1005</td>
+                                <td>SC</td>
+                                <td>57</td>
+                                <td>ENFERMERIA</td>
+                                <td>4630</td>
+                                <td>MOR-212</td>
+                                <td>MORFOLOGÍA II</td>
+                                <td>A</td>
+                                <td>NORE</td>
+                                <td>REGULAR</td>
+                                <td><a href="http://sis04.udabol.com/ingreso_nota/actas/ingresar/SC/57/4630/A/NORE">ingresar notas</a></td>                                
+                            </tr>     
+                            <tr>
+                                <td>1005</td>
+                                <td>SC</td>
+                                <td>57</td>
+                                <td>ENFERMERIA</td>
+                                <td>4630</td>
+                                <td>MOR-212</td>
+                                <td>MORFOLOGÍA II</td>
+                                <td>A1</td>
+                                <td>PRNO</td>
+                                <td>PRACTICO REGULAR</td>
+                                <td><a href="http://sis04.udabol.com/ingreso_nota/actas/ingresar/SC/57/4630/A1/PRNO">ingresar notas</a></td>                                
+                            </tr>     
+                            <tr>
+                                <td>1005</td>
+                                <td>SC</td>
+                                <td>57</td>
+                                <td>ENFERMERIA</td>
+                                <td>4630</td>
+                                <td>MOR-212</td>
+                                <td>MORFOLOGÍA II</td>
+                                <td>A2</td>
+                                <td>PRNO</td>
+                                <td>PRACTICO REGULAR</td>
+                                <td><a href="http://sis04.udabol.com/ingreso_nota/actas/ingresar/SC/57/4630/A2/PRNO">ingresar notas</a></td>                                
+                            </tr>     
+                            <tr>
+                                <td>1005</td>
+                                <td>SC</td>
+                                <td>99</td>
+                                <td>FISIOTERAPIA Y KINESIOLOGÍA</td>
+                                <td>4655</td>
+                                <td>FYK-312</td>
+                                <td>FISIOLOGÍA II</td>
+                                <td>A</td>
+                                <td>NORE</td>
+                                <td>REGULAR</td>
+                                <td><a href="http://sis04.udabol.com/ingreso_nota/actas/ingresar/SC/99/4655/A/NORE">ingresar notas</a></td>                                
+                            </tr>     
+                            <tr>
+                                <td>1005</td>
+                                <td>SC</td>
+                                <td>99</td>
+                                <td>FISIOTERAPIA Y KINESIOLOGÍA</td>
+                                <td>4655</td>
+                                <td>FYK-312</td>
+                                <td>FISIOLOGÍA II</td>
+                                <td>A1</td>
+                                <td>PRNO</td>
+                                <td>PRACTICO REGULAR</td>
+                                <td><a href="http://sis04.udabol.com/ingreso_nota/actas/ingresar/SC/99/4655/A1/PRNO">ingresar notas</a></td>                                
+                            </tr>     
+                            <tr>
+                                <td>1005</td>
+                                <td>SC</td>
+                                <td>99</td>
+                                <td>FISIOTERAPIA Y KINESIOLOGÍA</td>
+                                <td>4655</td>
+                                <td>FYK-312</td>
+                                <td>FISIOLOGÍA II</td>
+                                <td>A2</td>
+                                <td>PRNO</td>
+                                <td>PRACTICO REGULAR</td>
+                                <td><a href="http://sis04.udabol.com/ingreso_nota/actas/ingresar/SC/99/4655/A2/PRNO">ingresar notas</a></td>                                
+                            </tr>     
+                            <tr>
+                                <td>1005</td>
+                                <td>SC</td>
+                                <td>99</td>
+                                <td>FISIOTERAPIA Y KINESIOLOGÍA</td>
+                                <td>4655</td>
+                                <td>FYK-312</td>
+                                <td>FISIOLOGÍA II</td>
+                                <td>B</td>
+                                <td>NORE</td>
+                                <td>REGULAR</td>
+                                <td><a href="http://sis04.udabol.com/ingreso_nota/actas/ingresar/SC/99/4655/B/NORE">ingresar notas</a></td>                                
+                            </tr>     
+                            <tr>
+                                <td>1005</td>
+                                <td>SC</td>
+                                <td>99</td>
+                                <td>FISIOTERAPIA Y KINESIOLOGÍA</td>
+                                <td>4655</td>
+                                <td>FYK-312</td>
+                                <td>FISIOLOGÍA II</td>
+                                <td>B1</td>
+                                <td>PRNO</td>
+                                <td>PRACTICO REGULAR</td>
+                                <td><a href="http://sis04.udabol.com/ingreso_nota/actas/ingresar/SC/99/4655/B1/PRNO">ingresar notas</a></td>                                
+                            </tr>     
+                            <tr>
+                                <td>1005</td>
+                                <td>SC</td>
+                                <td>99</td>
+                                <td>FISIOTERAPIA Y KINESIOLOGÍA</td>
+                                <td>4655</td>
+                                <td>FYK-312</td>
+                                <td>FISIOLOGÍA II</td>
+                                <td>B2</td>
+                                <td>PRNO</td>
+                                <td>PRACTICO REGULAR</td>
+                                <td><a href="http://sis04.udabol.com/ingreso_nota/actas/ingresar/SC/99/4655/B2/PRNO">ingresar notas</a></td>                                
+                            </tr>     
+                        </tbody>
+                    </table>
+                </div>
             </section>
             <footer class="footer"><div>Todos los Derechos Reservados, Udabol © 2013</div></footer>
         </section>
